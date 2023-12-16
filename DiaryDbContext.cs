1f88f690bc7cbb2b200c3c03d22822990286f033
Linh Nguyen <linhtunguyen.1311@gmail.com>
2023-12-16 13:12:11 +1030

    Reconstruct the SQL database to reduce unnecessary code in DbContext

--- DiaryDbContext.cs
+++ DiaryDbContext.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 
 namespace DiaryApp;
 
@@ -29,56 +27,34 @@ public partial class DiaryDbContext : DbContext
         {
             entity.HasKey(e => e.Id).HasName("PRIMARY");
 
-            entity.ToTable("diaries");
+            entity.ToTable("Diary");
 
-            entity.HasIndex(e => e.UserId, "user_id");
+            entity.HasIndex(e => e.UserId, "UserId");
 
-            entity.Property(e => e.Id).HasColumnName("id");
-            entity.Property(e => e.Content)
-                .HasColumnType("text")
-                .HasColumnName("content");
+            entity.Property(e => e.Content).HasColumnType("text");
             entity.Property(e => e.CreatedTimestamp)
                 .HasDefaultValueSql("CURRENT_TIMESTAMP")
-                .HasColumnType("timestamp")
-                .HasColumnName("created_timestamp");
-            entity.Property(e => e.IsPublished).HasColumnName("is_published");
-            entity.Property(e => e.PublishedTimestamp)
-                .HasColumnType("timestamp")
-                .HasColumnName("published_timestamp");
-            entity.Property(e => e.Title)
-                .HasMaxLength(255)
-                .HasColumnName("title");
-            entity.Property(e => e.UserId).HasColumnName("user_id");
+                .HasColumnType("timestamp");
+            entity.Property(e => e.PublishedTimestamp).HasColumnType("timestamp");
+            entity.Property(e => e.Title).HasMaxLength(255);
 
             entity.HasOne(d => d.User).WithMany(p => p.Diaries)
                 .HasForeignKey(d => d.UserId)
                 .OnDelete(DeleteBehavior.ClientSetNull)
-                .HasConstraintName("diaries_ibfk_1");
+                .HasConstraintName("diary_ibfk_1");
         });
 
         modelBuilder.Entity<User>(entity =>
         {
             entity.HasKey(e => e.Id).HasName("PRIMARY");
 
-            entity.ToTable("users");
-
-            entity.Property(e => e.Id).HasColumnName("id");
             entity.Property(e => e.CreatedTimestamp)
                 .HasDefaultValueSql("CURRENT_TIMESTAMP")
-                .HasColumnType("timestamp")
-                .HasColumnName("created_timestamp");
-            entity.Property(e => e.Email)
-                .HasMaxLength(100)
-                .HasColumnName("email");
-            entity.Property(e => e.FirstName)
-                .HasMaxLength(50)
-                .HasColumnName("first_name");
-            entity.Property(e => e.HashedPassword)
-                .HasMaxLength(100)
-                .HasColumnName("hashed_password");
-            entity.Property(e => e.LastName)
-                .HasMaxLength(50)
-                .HasColumnName("last_name");
+                .HasColumnType("timestamp");
+            entity.Property(e => e.Email).HasMaxLength(100);
+            entity.Property(e => e.FirstName).HasMaxLength(50);
+            entity.Property(e => e.HashedPassword).HasMaxLength(100);
+            entity.Property(e => e.LastName).HasMaxLength(50);
         });
 
         OnModelCreatingPartial(modelBuilder);
